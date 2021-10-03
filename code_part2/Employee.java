e4a49e050332d80a804b8705db8b1ad2a8b57973
Riley de Gans <rdega009@uottawa.ca>
2021-10-02 21:39:24 -0400

    Second commit to part2

---
+++ code_part2/Employee.java
@@ -0,0 +1,13 @@
+
+public class Employee {
+private String name;
+private int hours;
+private double rate;
+private Address[] address;
+public Employee(String name, int hours, double rate, Address[] address) {
+	this.name = name;
+	this.hours = hours;
+	this.rate = rate;
+	this.address = address;
+}
+}
